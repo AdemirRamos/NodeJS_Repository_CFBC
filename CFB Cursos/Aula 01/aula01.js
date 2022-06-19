e44ff40787d39d931b8e9cddf92bcd63bbc677b9
AdemirRamos <ademirramos634@gmail.com>
2022-06-19 10:30:24 -0300

    Atualização da Aula 01

--- CFB Cursos/Aula 01/aula01.js
+++ CFB Cursos/Aula 01/aula01.js
@@ -1,3 +1,6 @@
-//Aula 01 - Vamos Aprender Node JS? (Introdução, Instalação, Primeira Aplicação Node JS).
+//Aula 01 - Vamos Aprender Node JS? (Introdução, instalação e primeira aplicação Node JS).
 
 console.log('Olá, mundo!')
+
+//O comando (sem aspas) "node nome do arquivo" faz com que o arquivo selecionado seja executado no Terminal.
+//O mesmo vale para o Prompt de Comando e Windows Terminal - em todo caso, é preciso estar dentro da pasta do arquivo a ser executado.
