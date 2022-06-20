543229b0735ea88d09bf2d35d2a57ff4e630bffa
AdemirRamos <ademirramos634@gmail.com>
2022-06-20 16:54:17 -0300

    Adição da Aula 06

---
+++ CFB Cursos/Aula 06/aula06.js
@@ -0,0 +1,11 @@
+/*
+
+    Aula 06 - Usando Node JS No Replit Sem Instalar Nada No PC
+
+    Link da API: https://replit.com/@AdemirRamos1/CFB-Cursos-Node-JS-Aula-06-API-de-Exemplo#index.js
+
+    Link da API com Express: https://CFB-Cursos-Node-JS-Aula-06-API-de-Exemplo-2.ademirramos1.repl.co
+
+    P. S.: No Replit, caso seja necessário, a instalação de pacotes será feita automaticamente e on-line.
+
+*/
