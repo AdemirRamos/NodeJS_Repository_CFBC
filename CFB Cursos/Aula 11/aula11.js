b9ec0288e5c44d8701f8d0bcda3415e1a55ffda7
Ademir Ramos <93046432+AdemirRamos@users.noreply.github.com>
2022-06-25 16:20:08 -0300

    A

--- CFB Cursos/Aula 11/aula11.js
+++ CFB Cursos/Aula 11/aula11.js
@@ -21,4 +21,32 @@ const servidor = http.createServer((requisição, resposta) => {
     resposta.end()
 })
 
+//Node JS Events: //Aula 11 - Trabalhando Com Eventos Em Node - Módulo "mysql2"
+
+const http = require('http')
+const porta = process.env.PORT || 3000
+const retorno = () => {console.log('O servidor está rodando.')}
+
+const eventos = require('events')
+const evento_emissor = new eventos.EventEmitter()
+const final = () => {console.log('Fim do processo.')}
+
+evento_emissor.on('mensagem', () => {console.log('Curso de Node JS.')})
+evento_emissor.on('fim', final)
+
+const servidor = http.createServer((requisição, resposta) => {
+    resposta.writeHead(200, {'Content-Type': 'text/plain'})
+    resposta.write('CFB Cursos')
+
+    evento_emissor.emit('mensagem')
+    evento_emissor.emit('fim')
+
+    resposta.end()
+})
+
+//Node Js Events: https://www.w3schools.com/nodejs/nodejs_events.asp
+
+servidor.listen(porta, retorno)
+
+
 servidor.listen(porta, retorno)
